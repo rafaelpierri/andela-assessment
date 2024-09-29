47cb1af9816190d3386f08a2d1747ecf1bff9389
Rafael Pierri <pierri.rafa@gmail.com>
2024-09-29 00:35:26 -0300

    Removes hello world api route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 import { HealthCheck, HealthCheckService, TypeOrmHealthIndicator } from '@nestjs/terminus';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
     private health: HealthCheckService,
     private db: TypeOrmHealthIndicator,
   ) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 
   @Get('/health')
   @HealthCheck()
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TerminusModule } from '@nestjs/terminus';
 import { AppDataSource } from '../data-source';
@@ -19,6 +18,5 @@ import { Product } from './product-catalog/infrastructure/product.entity';
     ProductSearchModule
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
